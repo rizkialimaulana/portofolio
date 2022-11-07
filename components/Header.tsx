050938f5646ac28f1af6d5b8f3571b243b373f9e
rizkialimaulana <maulanarizkiali@gmail.com>
2022-11-07 11:42:14 +0700

    Fix fade

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,8 +6,7 @@ type Props = {}
 
 const Header = (props: Props) => {
   return (
-    <div className='h-screen flex flex-col bg-gray-500/30 bg-fixed items-center overflow-hidden justify-center'>
-      <h1 className='font-Stretch text-left w-full px-8 md:px-16 text-xl'>PORTOFOLIOO</h1>
+    <div className="h-screen flex flex-col bg-gray-500/30 bg-fixed items-center overflow-hidden justify-center">
       <Image
         src="/profile-removebg.png"
         alt=""
@@ -15,17 +14,22 @@ const Header = (props: Props) => {
         height={300}
         className="relative w-[25rem] h-auto"
       />
-      <div className="absolute z-10 flex h-full items-end justify-between w-full px-8 md:px-16 pb-20">
-        <h1 className="text-4xl md:text-8xl font-Conthrax text-white">
-          Rizki Ali Maulana
+      <div className="w-full h-full absolute z-10 flex flex-col justify-between">
+        <h1 className="font-Stretch text-left px-8 md:px-16 text-xl">
+          PORTOFOLIOO
         </h1>
-        <div className="flex flex-col items-start text-lg md:text-3xl font-Stretch text-white">
-          <span>Frontend Developer</span>
-          <span>QA Engineer</span>
-          <span>Freelancer</span>
+        <div className="items-end justify-between px-8 md:px-16 pb-20">
+          <h1 className="text-4xl md:text-8xl font-Conthrax text-white">
+            Rizki Ali Maulana
+          </h1>
+          <div className="flex flex-col items-start text-lg md:text-3xl font-Stretch text-white">
+            <span>Frontend Developer</span>
+            <span>QA Engineer</span>
+            <span>Freelancer</span>
+          </div>
         </div>
       </div>
-      <div className="h-full w-full absolute z-1 bg-gradient-to-b from-transparent via-transparent to-black" />
+      <div className="h-screen w-full absolute z-1 bg-gradient-to-b from-transparent via-transparent to-black" />
     </div>
   );
 }
